eb433614f159db776b0b360269135ada8e8cea4f
Tung <thahtung113@gmail.com>
2022-10-22 18:26:52 +0700

    fdssd

--- src/Component/Form2/Detail.js
+++ src/Component/Form2/Detail.js
@@ -2,29 +2,55 @@ import React from "react";
 import "./Form2.css";
 
 const Detail = (props) => {
+  console.log(props);
   return (
     <div className="container-detail">
       <div>
         <h1>To List</h1>
         <input className="input-detail" tyle="text" placeholder="Search..." />
+
         <div>
           {props.data.map((item) => {
             return (
               <div key={item.id}>
-              <div>{item.input}</div>
-              <button onClick={()=>props.detail(item.id)}>Detail
-                
-                {/* {props.detail(item.id) &&(
-                    <div>dfd</div>
-                )} */}
+                <div>{item.input}</div>
+                <input type="checkbox" name="check" onClick={()=>props.check(item.id)}/>
+                <button onClick={() => props.detail(item.id)}>
+                  Detail
                 </button>
-                <button onClick={()=>props.delete(item.id)}>Delete</button>
-                {props.visible.includes(item.id) && <div>2</div>}
+                <button onClick={() => props.delete(item.id)}>Delete</button>
+
+                {props.visible.includes(item.id) && (
+                  <div>
+                    <form
+                      // className="container-detail"
+                      onSubmit={(e) => props.edit(e, item.id)}
+                    >
+                      <input
+                        className="input-detail1"
+                        name="input"
+                        defaultValue={item.input}
+                      />
+                      <textarea
+                        className="input-detail1"
+                        name="des"
+                        defaultValue={item.des}
+                      />
+                      <input type="date" name="date" defaultValue={item.date} />
+                      <select name="piority" defaultValue={item.piority}>
+                        <option>Low</option>
+                        <option>Normal</option>
+                        <option>High</option>
+                      </select>
+                      <button type="submit">Edit</button>
+                    </form>
+                  </div>
+                )}
               </div>
-              
             );
           })}
         </div>
+        <button type="button" onClick={props.deleteAll}>DeleteAll</button>
       </div>
     </div>
   );
--- src/Component/Form2/Index.js
+++ src/Component/Form2/Index.js
@@ -1,12 +1,17 @@
-import React, { useState, useId } from "react";
+import React, { useState, useId, useEffect } from "react";
 import Detail from "./Detail";
 import Form2 from "./Form2";
 import "./Form2.css";
 
 const Index = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(() => {
+    let Initlocal;
+    Initlocal = JSON.parse(localStorage.getItem("myForm")) || [];
+    return Initlocal;
+  });
   const [count, setCount] = useState(0);
   const [visible, setVisible] = useState([]);
+
   const handelete = (id) => {
     const newData = data.filter((item) => {
       return item.id !== id;
@@ -24,21 +29,58 @@ const Index = () => {
     }
     setVisible([...visible, id]);
   };
-  console.log(visible, "zz");
+
+  const handleEdit = (e, id) => {
+    e.preventDefault();
+    const newData = [...data];
+    if (e.target.input.value === "") return;
+    newData.forEach((item) => {
+      if (item.id === id) {
+        item.input = e.target[0].value;
+        item.des = e.target[1].value;
+        item.date = e.target[2].value;
+        item.piority = e.target[3].value;
+      }
+    });
+    setData(newData);
+  };
   const handleSubmit = (e) => {
+    if (e.target.input.value === "") return;
     const newData = {
       id: count,
       input: e.target.input.value,
       des: e.target.des.value,
       date: e.target.date.value,
       piority: e.target.piority.value,
+      check: true,
     };
     e.preventDefault();
     setData([...data, newData]);
     setCount((pre) => pre + 1);
+    e.target.reset();
   };
 
-  // const [detail,setDetail]=useState(false)
+  const handleComplete = (id) => {
+    const newData = [...data];
+    newData.forEach((item) => {
+      if (item.id === id) {
+        item.check = !item.check;
+      }
+    });
+    setData(newData);
+  };
+
+  const handleDeleteAll = () => {
+    const dd = data.filter((item) => {
+      return item.check;
+    });
+    setData(dd);
+  };
+
+  console.log(data);
+  useEffect(() => {
+    localStorage.setItem("myForm", JSON.stringify(data));
+  }, [data]);
 
   return (
     <div className="container-index">
@@ -48,7 +90,9 @@ const Index = () => {
         delete={handelete}
         detail={handleDetail}
         visible={visible}
-        // setdetail={detail}
+        edit={handleEdit}
+        check={handleComplete}
+        deleteAll={handleDeleteAll}
       />
     </div>
   );
